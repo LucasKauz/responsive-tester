b3a784c3e607a3bafc11eeaa5e633faa11655e01
Lucas Marques <lucaskauz@gmail.com>
2020-01-19 22:07:49 -0300

    feature: demo controller

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
+import Color from 'color'
 import './scss/main.scss'
+
+// Root
+const root = document.documentElement;
+
+
+class DeviceSwitcher {
+  
+  constructor (main) {
+    this.main = main
+  }
+
+  setPhone () {
+    this.main.classList.remove('watch-mode')
+  }
+
+  setWatch () {
+    this.main.classList.add('watch-mode')
+  }
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+  const deviceSwitcher = new DeviceSwitcher(document.body)
+
+  const colorBtns = [...document.getElementsByClassName('ColorSwitcher__btn')]
+  colorBtns.forEach(el => {
+    el.style.backgroundColor = el.dataset['color']
+    el.addEventListener('click', (e) => {
+      const btnColor = e.target.dataset['color']
+
+      root.style.setProperty('--base', btnColor)
+      root.style.setProperty('--base-200', Color(btnColor).darken(0.3))
+    })
+  })
+
+  document.getElementById('SwitchPhone').addEventListener('click', () => {
+    deviceSwitcher.setPhone()
+  })
+
+  document.getElementById('SwitchWatch').addEventListener('click', () => {
+    deviceSwitcher.setWatch()
+  })
+})
